9b47939366fc27c1f555258c94d752a73ffb4e18
Oleg <codingoleg@proton.me>
2023-10-12 12:50:06 +0300

    Update: Flask and DB parameters

--- config.py
+++ config.py
@@ -1,14 +1,16 @@
+# Параметры Flask
+FLASK_HOST = '127.0.0.1'
+FLASK_PORT = 5000
+
 # Параметры БД
-DB_HOST: str = 'localhost'
+# DB_HOST должен быть таким же, как название сервиса в docker-compose.yml
+# Либо DB_HOST = 'localhost' для локального запуска.
+DB_HOST: str = 'mongodb'
 DB_PORT: int = 27017
 DB_NAME: str = 'db'
 COLLECTION_NAME: str = 'issue_system'
 
-# Параметры БД для тестов
-TEST_DB_NAME: str = 'test_db'
-TEST_COLLECTION_NAME: str = 'test_issue_system'
-
-# Ключи
+# Названия ключей
 BODY: str = 'body'
 HEADERS: str = 'headers'
 HASH: str = 'hash'
@@ -19,7 +21,7 @@ ROUTE_FIND: str = '/find'
 ROUTE_FIND2: str = '/find2'
 
 # URLs
-URL_INDEX: str = "http://127.0.0.1:5000"
+URL_INDEX: str = f"http://{FLASK_HOST}:{FLASK_PORT}"
 URL_PROBLEMS: str = URL_INDEX + ROUTE_PROBLEMS
 URL_FIND: str = URL_INDEX + ROUTE_FIND
 URL_FIND2: str = URL_INDEX + ROUTE_FIND2
